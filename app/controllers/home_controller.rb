31e4f97da427f87986f270e400ba04c0d31fdf4d
edifierd <fedetubaro@hotmail.com>
2015-11-11 02:30:11 -0300

    Se acomodo un poco las vistas para subirlo a Heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ActionController::Base
 
   def index
-  	if current_user
-  		@ability = Ability.new(current_user)
-  	end
+  	#if current_user
+  	#	@ability = Ability.new(current_user)
+  	#end
   end
 
   #def show
